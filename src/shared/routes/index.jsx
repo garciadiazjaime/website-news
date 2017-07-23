a7d3e5c4af067b80b8cacefce3c628159172d73c
garciadiazjaime <garciadiazjaime@gmail.com>
2017-07-23 13:49:28 -0500

    setup app

---
+++ src/shared/routes/index.jsx
@@ -0,0 +1,13 @@
+/* eslint max-len: [2, 500, 4] */
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+import { BrowserRouter } from 'react-router-dom'
+
+import App from '../components/app';
+
+export default(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
