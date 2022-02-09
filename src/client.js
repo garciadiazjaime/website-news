b7cd75e6fedc5aced86ec1322b44595723052ea9
jaime <garciadiazjaime@gmail.com>
2022-02-08 22:10:57 -0600

    fix: lcp

--- src/client.js
+++ src/client.js
@@ -1,15 +1,5 @@
 import * as sapper from '@sapper/app';
 
-const target = document.querySelector('#sapper')
-
-if (!target) {
-	setTimeout(init, 0)
-} else {
-	init()
-}
-
-function init() {
-	sapper.start({
-		target: document.querySelector('#sapper')
-	});
-}
+sapper.start({
+	target: document.querySelector('#sapper')
+});
